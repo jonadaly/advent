76eeb48c57908f47a58659163059256d2f528155
Jon Daly <jonathan.daly@sensynehealth.com>
2021-12-21 11:00:50 +0000

    Bask in day 19

--- 2021/calendar-19.py
+++ 2021/calendar-19.py
@@ -3,19 +3,17 @@ import math
 import re
 from collections import deque, defaultdict
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict
 
 import numpy as np
-from numpy import ndarray
 
-scanners_raw = Path("19-example.txt").read_text().strip().split("\n\n")
+scanners_raw = Path("19.txt").read_text().strip().split("\n\n")
 
+# Create the 24 possible rotation matrices.
 # https://www.andre-gaschler.com/rotationconverter/
 rotate_x = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
 rotate_y = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
 rotate_z = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
-
-# Generate 24 rotations
 rotations = []
 rotations_str = []
 for i in range(4):
@@ -30,10 +28,11 @@ for i in range(4):
                 rotations.append(ovr_rot)
                 rotations_str.append(str(ovr_rot))
 
+# Build the scanners.
 scanners = []
 for scanner_raw in scanners_raw:
-    pairs = {}
-    point_fingerprints: Dict[tuple, list] = defaultdict(list)
+    distances = []
+    fingerprints: Dict[tuple, list] = defaultdict(list)
     raw_header, *raw_coord_lines = scanner_raw.split("\n")
     scanner_id = int(re.match(r"^--- scanner (\d+) ---$", raw_header).groups()[0])
     coordinates = [tuple(map(int, r.split(","))) for r in raw_coord_lines]
@@ -43,36 +42,32 @@ for scanner_raw in scanners_raw:
         x1, y1, z1 = tuple(map(int, c1.split(",")))
         x2, y2, z2 = tuple(map(int, c2.split(",")))
         distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
-        pairs[((x1, y1, z1), (x2, y2, z2))] = distance
-        point_fingerprints[(x1, y1, z1)].append(distance)
-        point_fingerprints[(x2, y2, z2)].append(distance)
-    scanners.append({"id": scanner_id, "pairs": pairs, "raw_coordinates": coordinates, "point_fingerprints": point_fingerprints})
+        distances.append(distance)
+        fingerprints[(x1, y1, z1)].append(distance)
+        fingerprints[(x2, y2, z2)].append(distance)
+    scanners.append({"id": scanner_id, "distances": distances, "raw_coordinates": coordinates, "fingerprints": fingerprints})
 
 scanners[0]["aligned_coordinates"] = scanners[0]["raw_coordinates"].copy()
-scanners[0]["aligned_point_fingerprints"] = scanners[0]["point_fingerprints"].copy()
+scanners[0]["scanner_location"] = (0, 0, 0)
+scanners[0]["aligned_fingerprints"] = scanners[0]["fingerprints"].copy()
 aligned = [scanners[0]]
 remaining = deque(scanners[1:])
 while remaining:
-    print("iterating")
     scanner_to_align = remaining.popleft()
     match_found = False
     for a in aligned:
-        in_common = set(a["pairs"].values()) & set(scanner_to_align["pairs"].values())
+        in_common = set(a["distances"]) & set(scanner_to_align["distances"])
         if len(in_common) >= math.comb(12, 2):
             # Found a match.
             match_found = True
-            print(f"match found between scanner {scanner_to_align['id']} and {a['id']}")
 
             # alignment
             matched_points = []
             for p1 in scanner_to_align["raw_coordinates"]:
-                distances = scanner_to_align['point_fingerprints'][p1]
+                distances = scanner_to_align['fingerprints'][p1]
                 for p2 in a["aligned_coordinates"]:
-                    # print(len(set(distances) & set(a["point_fingerprints"][p2])))
-                    if len(set(distances) & set(a["aligned_point_fingerprints"][p2])) >= 11:
-                        print(f"MATCH: {p1} and {p2}")
+                    if len(set(distances) & set(a["aligned_fingerprints"][p2])) >= 11:
                         matched_points.append((p1, p2))
-            print(len(matched_points))
             assert len(matched_points) >= 12
 
             # y = ax + b (a is the rotation matrix, b is the translation matrix)
@@ -80,50 +75,47 @@ while remaining:
             y = np.array([i[1] for i in matched_points])
             correct_rotation = None
             offset = None
-            for i, rot in enumerate(rotations):
-                # print(i)
-                # print(x)
-                # print(rot)
-                # print("AFTER")
-                # print(x@rot)
-                # print((x@rot).flatten())
+            for rot in rotations:
                 transformed = np.subtract(y, x@rot)
-                print(len(set(transformed.flatten())))
                 if len(set(transformed.flatten())) <= 3:
                     # found the matching rotation.
                     correct_rotation = rot
                     offset = transformed[0]
-
-            # p = scanner_to_align["raw_coordinates"][0]
-            # print(p)
-            # t = np.subtract(offset, p @ correct_rotation)
-            # print(t)
-            # exit()
+                    break
 
             coordinates_lookup = {
-                p: tuple(np.subtract(offset, p@correct_rotation)) for p in scanner_to_align["raw_coordinates"]
+                p: tuple(np.add(offset, p@correct_rotation)) for p in scanner_to_align["raw_coordinates"]
             }
 
             aligned_scanner = {
                 "id": scanner_to_align["id"],
-                "pairs": scanner_to_align["pairs"],
+                "distances": scanner_to_align["distances"],
                 "raw_coordinates": scanner_to_align["raw_coordinates"],
                 "aligned_coordinates": [coordinates_lookup[c] for c in scanner_to_align["raw_coordinates"]],
-                "aligned_pairs": {(coordinates_lookup[k[0]], coordinates_lookup[k[1]]): v for k, v in
-                          scanner_to_align["pairs"].items()},
-                "aligned_point_fingerprints": {coordinates_lookup[k]:v for k, v in scanner_to_align["point_fingerprints"].items()},
-                "point_fingerprints": scanner_to_align["point_fingerprints"],
-                "scanner_location": offset
+                "aligned_fingerprints": {coordinates_lookup[k]:v for k, v in scanner_to_align["fingerprints"].items()},
+                "fingerprints": scanner_to_align["fingerprints"],
+                "scanner_location": offset,
             }
 
             aligned.append(aligned_scanner)
-            # print(aligned_scanner["raw_coordinates"])
-            # print(aligned_scanner["scanner_location"])
-
+            print(f'Scanner {aligned_scanner["id"]} at {aligned_scanner["scanner_location"]}')
             break
+
     if not match_found:
         print(f"No match found for scanner {scanner_to_align['id']}")
         remaining.append(scanner_to_align)
+
+all_beacons = set().union(*[s["aligned_coordinates"] for s in aligned])
+print(f"Part 1: there are {len(all_beacons)} beacons in total")
+
+scanner_locations = [s["scanner_location"] for s in aligned]
+max_manhattan = 0
+for b1, b2 in itertools.combinations(scanner_locations, r=2):
+    manhattan_distance = abs(b1[0] - b2[0]) + abs(b1[1] - b2[1]) + abs(b1[2] - b2[2])
+    max_manhattan = max(max_manhattan, manhattan_distance)
+print(f"Part 2: max distance between scanners is {max_manhattan}")
+
+
 
 
 
