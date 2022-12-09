57641720d79cb8292d643316eec87090a444164e
Jon Daly <jon.daly@joinzoe.com>
2022-12-09 14:55:31 +0000

    Simplify even more

--- advent_2022/day-09.py
+++ advent_2022/day-09.py
@@ -3,6 +3,10 @@ from pathlib import Path
 instructions: list[str] = Path("day-09-input.txt").read_text().strip().split("\n")
 
 
+def move(start: tuple[int, int], vector: tuple[int, int]) -> tuple[int, int]:
+    return (start[0] + vector[0], start[1] + vector[1])
+
+
 def track_tail(knots: int) -> set[tuple[int, int]]:
     visited: set[tuple[int, int]] = {(0, 0)}
     pos = [(0, 0) for _ in range(knots)]
@@ -11,35 +15,35 @@ def track_tail(knots: int) -> set[tuple[int, int]]:
         for _ in range(int(amount)):
             match (direction):
                 case "R":
-                    pos[0] = (pos[0][0] + 1, pos[0][1])
+                    pos[0] = move(pos[0], (1, 0))
                 case "L":
-                    pos[0] = (pos[0][0] - 1, pos[0][1])
+                    pos[0] = move(pos[0], (-1, 0))
                 case "U":
-                    pos[0] = (pos[0][0], pos[0][1] + 1)
+                    pos[0] = move(pos[0], (0, 1))
                 case "D":
-                    pos[0] = (pos[0][0], pos[0][1] - 1)
+                    pos[0] = move(pos[0], (0, -1))
             # Update tail
             for i in range(1, knots):
                 diff = (pos[i - 1][0] - pos[i][0], pos[i - 1][1] - pos[i][1])
                 match (diff):
-                    case (0, 2):
-                        pos[i] = (pos[i][0], pos[i][1] + 1)
-                    case (0, -2):
-                        pos[i] = (pos[i][0], pos[i][1] - 1)
                     case (2, 0):
-                        pos[i] = (pos[i][0] + 1, pos[i][1])
+                        pos[i] = move(pos[i], (1, 0))
                     case (-2, 0):
-                        pos[i] = (pos[i][0] - 1, pos[i][1])
+                        pos[i] = move(pos[i], (-1, 0))
+                    case (0, 2):
+                        pos[i] = move(pos[i], (0, 1))
+                    case (0, -2):
+                        pos[i] = move(pos[i], (0, -1))
                     case (1, 2) | (2, 1) | (2, 2):
-                        pos[i] = (pos[i][0] + 1, pos[i][1] + 1)
-                    case (1, -2) | (2, -1) | (2, -2):
-                        pos[i] = (pos[i][0] + 1, pos[i][1] - 1)
+                        pos[i] = move(pos[i], (1, 1))
                     case (-1, 2) | (-2, 1) | (-2, 2):
-                        pos[i] = (pos[i][0] - 1, pos[i][1] + 1)
+                        pos[i] = move(pos[i], (-1, 1))
+                    case (1, -2) | (2, -1) | (2, -2):
+                        pos[i] = move(pos[i], (1, -1))
                     case (-1, -2) | (-2, -1) | (-2, -2):
-                        pos[i] = (pos[i][0] - 1, pos[i][1] - 1)
+                        pos[i] = move(pos[i], (-1, -1))
                     case _:
-                        "adjacent - no-op"
+                        pass  # adjacent
             visited.add(pos[-1])
     return visited
 
