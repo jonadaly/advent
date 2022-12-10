1bc4f85ad984d0281aa55c3e1a10d346eb3c0455
Jon Daly <jon.daly@joinzoe.com>
2022-12-10 12:13:26 +0000

    Extract magic numbers

--- advent_2022/day-10.py
+++ advent_2022/day-10.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 import numpy as np
 
+CYCLES = (20, 60, 100, 140, 180, 220)
+WIDTH = 40
+HEIGHT = 6
+
 instructions: list[str] = Path("day-10-input.txt").read_text().strip().split("\n")
 
 changes: list[int] = [1]
@@ -11,14 +15,14 @@ for instruction in instructions:
     if instruction != "noop":
         changes.append(int(instruction.split(" ")[1]))
 signal_strengths: np.ndarray = np.cumsum(changes)
-total = sum(i * signal_strengths[i - 1] for i in [20, 60, 100, 140, 180, 220])
+total = sum(i * signal_strengths[i - 1] for i in CYCLES)
 print(f"Part 1: Total signal strength is {total}")
 
 # Build screen repr.
-screen: list[list[str]] = [["."] * 40 for _ in range(6)]
+screen: list[list[str]] = [["."] * WIDTH for _ in range(HEIGHT)]
 
 # Fancy double for-loop with enumeration for the cycle number.
-for cycle, (i_row, i_col) in enumerate(product(range(6), range(40))):
+for cycle, (i_row, i_col) in enumerate(product(range(HEIGHT), range(WIDTH))):
     pos = signal_strengths[cycle]
     screen[i_row][i_col] = "#" if i_col in [pos - 1, pos, pos + 1] else "."
 
