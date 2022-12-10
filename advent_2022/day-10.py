3436bff01a1b86941a41eb4eb30eed1db57d2b36
Jon Daly <jon.daly@joinzoe.com>
2022-12-10 12:07:57 +0000

    Tidy up

--- advent_2022/day-10.py
+++ advent_2022/day-10.py
@@ -14,12 +14,13 @@ signal_strengths: np.ndarray = np.cumsum(changes)
 total = sum(i * signal_strengths[i - 1] for i in [20, 60, 100, 140, 180, 220])
 print(f"Part 1: Total signal strength is {total}")
 
-crt_screen: list[list[str]] = [["."] * 40 for _ in range(6)]
+# Build screen repr.
+screen: list[list[str]] = [["."] * 40 for _ in range(6)]
 
 # Fancy double for-loop with enumeration for the cycle number.
 for cycle, (i_row, i_col) in enumerate(product(range(6), range(40))):
-    sprite = signal_strengths[cycle]
-    crt_screen[i_row][i_col] = "#" if i_col in [sprite - 1, sprite, sprite + 1] else "."
+    pos = signal_strengths[cycle]
+    screen[i_row][i_col] = "#" if i_col in [pos - 1, pos, pos + 1] else "."
 
 print("Part 2:")
-print("\n".join("".join(row) for row in crt_screen))
+print("\n".join("".join(row) for row in screen))
