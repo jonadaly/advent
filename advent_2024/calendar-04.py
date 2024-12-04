ac1daa8cd8540e8aad6f309b0831bb7bcfa8bf80
Jon Daly <jon.daly@joinzoe.com>
2024-12-04 10:28:56 +0000

    Tidy

--- advent_2024/calendar-04.py
+++ advent_2024/calendar-04.py
@@ -7,17 +7,20 @@ grid = np.array([list(line) for line in raw_input], dtype=str)
 
 # Part 1 - Word-search for "XMAS". There's a more efficient way (scan for matching tiles), but that's boring.
 grid_rotated = grid[::-1].T
-grid_diagonals = [
-    np.diagonal(grid[i:, :], axis1=0, axis2=1)
-    for i in reversed(range(1, grid.shape[0]))
-] + [np.diagonal(grid[:, i:], axis1=0, axis2=1) for i in range(0, grid.shape[0])]
-grid_diagonals_other = [
-    np.diagonal(grid_rotated[i:, :], axis1=0, axis2=1)
-    for i in reversed(range(1, grid_rotated.shape[0]))
-] + [
-    np.diagonal(grid_rotated[:, i:], axis1=0, axis2=1)
-    for i in range(0, grid_rotated.shape[0])
-]
+
+
+def get_diagonals(char_array: np.ndarray) -> list[np.ndarray]:
+    return [
+        np.diagonal(char_array[i:, :], axis1=0, axis2=1)
+        for i in reversed(range(1, char_array.shape[0]))
+    ] + [
+        np.diagonal(char_array[:, i:], axis1=0, axis2=1)
+        for i in range(0, char_array.shape[0])
+    ]
+
+
+grid_diagonals = get_diagonals(grid)
+grid_diagonals_other = get_diagonals(grid_rotated)
 dir1 = ["".join(r) for r in grid]
 dir2 = ["".join(r) for r in grid_rotated]
 dir3 = ["".join(r) for r in grid[:, ::-1]]
@@ -42,7 +45,6 @@ base_tile = np.array(
 )
 tiles = [np.rot90(base_tile, i) for i in range(4)]
 mask = base_tile == "."
-
 total = 0
 for i in range(grid.shape[0] - 2):
     for j in range(grid.shape[1] - 2):
