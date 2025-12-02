fcfe5b9b1c7bd1b421bec41dcfcbe2f335fdbf80
Jon Daly <jon.daly@joinzoe.com>
2025-12-02 13:14:09 +0000

    Improve day2 performance by 60%

--- advent_2025/day02/src/main.rs
+++ advent_2025/day02/src/main.rs
@@ -2,14 +2,14 @@ use std::fs;
 
 fn has_repeating_chunks(id_str: &str, chunk_size: usize) -> bool {
     // If all subsequent chunks are equal to chunk1, then id is invalid.
-    let chunk1 = id_str[..chunk_size].to_string();
+    let chunk1 = &id_str[..chunk_size];
     for chunk_idx in 1..id_str.len() / chunk_size {
-        let chunk2 = id_str[chunk_idx * chunk_size..(chunk_idx + 1) * chunk_size].to_string();
+        let chunk2 = &id_str[chunk_idx * chunk_size..(chunk_idx + 1) * chunk_size];
         if chunk2 != chunk1 {
             return false;
         }
     }
-    return true;
+    true
 }
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Could not read file");
@@ -25,16 +25,16 @@ fn main() {
     let mut p1_sum = 0;
     let mut p2_sum = 0;
     for range in ranges {
-        for id in range.0..range.1 + 1 {
+        for id in range.0..=range.1 {
             let id_str = id.to_string();
             let length = id_str.len();
-            let factors: Vec<usize> = (1..length / 2 + 1).filter(|x| length % x == 0).collect();
-            for chunk_size in factors {
+            for chunk_size in (1..=length / 2).filter(|x| length % x == 0) {
                 if has_repeating_chunks(&id_str, chunk_size) {
                     if length / chunk_size == 2 {
                         p1_sum += id;
                     }
                     p2_sum += id;
+                    break;
                 }
             }
         }
