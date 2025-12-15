9ad11a01be3941df52ee7b283c353e1721ed0d1b
Jon Daly <jon.daly@joinzoe.com>
2025-12-15 11:35:44 +0000

    Add day10 part 2

--- advent_2025/day10/src/main.rs
+++ advent_2025/day10/src/main.rs
@@ -1,8 +1,9 @@
+use itertools::Itertools;
 use regex::Regex;
-use std::{
-    collections::{HashSet, VecDeque},
-    fs,
-};
+use std::{collections::HashMap, fs};
+
+/// I originally solved part 1 with a BFS, but needed a lot of help to figure out a solution for part 2.
+/// https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
 
 struct Machine {
     desired_lights: Vec<bool>,
@@ -10,31 +11,61 @@ struct Machine {
     desired_joltages: Vec<usize>,
 }
 
-fn simplify(state: &[usize]) -> Vec<bool> {
-    state.iter().map(|x| x % 2 == 1).collect()
-}
-
-fn bfs(machine: &Machine) -> Option<usize> {
-    let initial: Vec<usize> = vec![0; machine.desired_lights.len()];
-    let mut visited = HashSet::new();
-    let mut queue = VecDeque::from([(initial, 0)]);
-
-    while let Some((state, depth)) = queue.pop_front() {
-        if !visited.insert(simplify(&state)) {
-            continue;
-        }
-        for button in &machine.buttons {
-            let mut next = state.clone();
-            for &light in button {
-                next[light] += 1;
+fn find_all_patterns_with_cost(
+    buttons: &[Vec<usize>],
+    num_vars: usize,
+) -> HashMap<Vec<usize>, usize> {
+    // Insight: it's only worth pressing each button 0 or 1 times (2 is the same as 0).
+    let mut patterns = HashMap::new();
+    for num_pressed in 0..=buttons.len() {
+        for indices in (0..buttons.len()).combinations(num_pressed) {
+            let mut pattern = vec![0; num_vars];
+            for i in indices {
+                for &light in &buttons[i] {
+                    pattern[light] += 1;
+                }
             }
-            if simplify(&next) == machine.desired_lights {
-                return Some(depth + 1);
-            }
-            queue.push_back((next, depth + 1));
+            // Cost is the number of button presses required.
+            patterns.entry(pattern).or_insert(num_pressed);
         }
     }
-    None
+    patterns
+}
+
+fn solve_recursive(
+    target: &[usize],
+    all_patterns: &HashMap<Vec<usize>, usize>,
+    memo: &mut HashMap<Vec<usize>, usize>,
+) -> usize {
+    if target.iter().all(|x| *x == 0) {
+        // We got to the target, no more button presses needed.
+        return 0;
+    }
+    if let Some(cached) = memo.get(target) {
+        return *cached;
+    }
+    let mut solution = usize::MAX;
+    for (pattern, cost) in all_patterns {
+        // Check we haven't gone too far, and that the parity is the same as the target.
+        let valid = pattern
+            .iter()
+            .zip(target.iter())
+            .all(|(&p, &g)| p <= g && p % 2 == g % 2);
+        if valid {
+            // Subtract pattern from target, divide by 2 (see Reddit post for explanation)
+            let new_target: Vec<usize> = pattern
+                .iter()
+                .zip(target)
+                .map(|(&p, &g)| (g - p) / 2)
+                .collect();
+            let sub_solution = solve_recursive(&new_target, all_patterns, memo);
+            if sub_solution < usize::MAX {
+                solution = solution.min(cost + 2 * sub_solution);
+            }
+        }
+    }
+    memo.insert(target.to_vec(), solution);
+    solution
 }
 
 fn main() {
@@ -66,10 +97,28 @@ fn main() {
         })
         .collect();
 
-    let mut total_button_presses = 0;
+    let mut total_button_presses_p1 = 0;
+    let mut total_button_presses_p2 = 0;
     for machine in machines {
-        let steps_p1 = bfs(&machine).unwrap();
-        total_button_presses += steps_p1;
+        let all_patterns =
+            find_all_patterns_with_cost(&machine.buttons, machine.desired_lights.len());
+        let button_presses_p1 = all_patterns
+            .iter()
+            .filter(|(pattern, _)| {
+                pattern
+                    .iter()
+                    .map(|p| p % 2 == 1)
+                    .eq(machine.desired_lights.iter().copied())
+            })
+            .map(|(_, &cost)| cost)
+            .min()
+            .unwrap();
+
+        total_button_presses_p1 += button_presses_p1;
+        let mut memo = HashMap::new();
+        let button_presses_p2 = solve_recursive(&machine.desired_joltages, &all_patterns, &mut memo);
+        total_button_presses_p2 += button_presses_p2;
     }
-    println!("Part 1: Total button presses: {}", total_button_presses);
+    println!("Part 1: Total button presses: {}", total_button_presses_p1); // 477
+    println!("Part 2: Total button presses: {}", total_button_presses_p2); // 17970
 }
